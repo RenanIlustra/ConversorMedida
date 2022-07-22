d6bea6d90308a27f53dd2ce1ea461310397c8f7f
RenanIlustra <renanrodrigues.ilustra@gmail.com>
2022-07-22 15:34:30 -0300

    Spinner Adapter

--- app/src/main/java/com/renan/conversordemedidas/MainActivity.kt
+++ app/src/main/java/com/renan/conversordemedidas/MainActivity.kt
@@ -2,13 +2,14 @@ package com.renan.conversordemedidas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import com.renan.conversordemedidas.model.CalculationStrategyHolder
 import com.renan.conversordemedidas.model.strategies.*
 
 class MainActivity : AppCompatActivity() {
 
-    val supportedCalculationStrategies = arrayOf(
+    private val supportedCalculationStrategies = arrayOf(
         CalculationStrategyHolder("Centimetro para Quilometro",CentimeterToKilometerStrategy()),
         CalculationStrategyHolder("Quilometro para Centimetro",KilometerToCentimeterStrategy()),
         CalculationStrategyHolder("Quilometro para Metro",KilometerToMeterStrategy()),
@@ -21,9 +22,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val spConversions: Spinner = findViewById(R.id.spConversions)
+        //Configurando e populando o spinner
+        val spAdapter = ArrayAdapter(this,R.layout.res_spinner_item, getSpinnerData())
+        spAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spConversions.adapter = spAdapter
+    }
+
+    private fun getSpinnerData():MutableList<String> {
         val spinnerData = mutableListOf<String>()
+        supportedCalculationStrategies.forEach {
+            spinnerData.add(it.name)
 
-        val teste : String
-
+        }
+        return spinnerData
     }
 }
