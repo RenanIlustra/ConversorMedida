a2254632c2a487bc89e41197c893ebaac184c9b4
RenanIlustra <renanrodrigues.ilustra@gmail.com>
2022-07-22 16:54:06 -0300

    Click Bottom Convert

--- app/src/main/java/com/renan/conversordemedidas/MainActivity.kt
+++ app/src/main/java/com/renan/conversordemedidas/MainActivity.kt
@@ -3,11 +3,17 @@ package com.renan.conversordemedidas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
 import com.renan.conversordemedidas.model.CalculationStrategyHolder
+import com.renan.conversordemedidas.model.Calculator
 import com.renan.conversordemedidas.model.strategies.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var spConversions: Spinner
 
     private val supportedCalculationStrategies = arrayOf(
         CalculationStrategyHolder("Centimetro para Quilometro",CentimeterToKilometerStrategy()),
@@ -22,7 +28,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spConversions: Spinner = findViewById(R.id.spConversions)
+        unitUi()
+        setUi()
+        setActions()
+
+    }
+
+    private fun unitUi() {
+        spConversions = findViewById(R.id.spConversions)
+
+    }
+
+    private fun setActions() {
+        val btnConvert : Button = findViewById(R.id.btnConvert)
+        val edtValue : EditText = findViewById(R.id.edtValue)
+
+        btnConvert.setOnClickListener {
+
+            try {
+                //Valor digitado no EditText
+                val value = edtValue.text.toString().toDouble()
+
+                //Criando uma variavel para Guardar a posição selecionada no spinner
+                val selectedItemPosition = spConversions.selectedItemPosition
+
+                //Pegando na mutableList criada de StrategyHolders a estrategia da posição selecionada pelo Spinner
+                val calculationStrategy = supportedCalculationStrategies[selectedItemPosition]
+
+                Calculator.setCalculationStrategy(calculationStrategy.calculationStrategy)
+
+                Calculator.calculate(value)
+
+
+            }catch (e:NumberFormatException){
+                edtValue.error = "Valor inválido"
+                edtValue.requestFocus()
+
+            }
+        }
+
+    }
+
+    private fun setUi() {
+        spConversions = findViewById(R.id.spConversions)
         //Configurando e populando o spinner
         val spAdapter = ArrayAdapter(this,R.layout.res_spinner_item, getSpinnerData())
         spAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
