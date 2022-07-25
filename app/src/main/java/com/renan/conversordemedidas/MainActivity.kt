b936c7698e7fc4072d313d2f1a6919e934aaeac5
RenanIlustra <renanrodrigues.ilustra@gmail.com>
2022-07-25 13:13:17 -0300

    Finish

--- app/src/main/java/com/renan/conversordemedidas/MainActivity.kt
+++ app/src/main/java/com/renan/conversordemedidas/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.renan.conversordemedidas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
@@ -10,16 +12,18 @@ import com.renan.conversordemedidas.model.CalculationStrategyHolder
 import com.renan.conversordemedidas.model.Calculator
 import com.renan.conversordemedidas.model.strategies.*
 import java.lang.NumberFormatException
+import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var edtValue: EditText
     private lateinit var spConversions: Spinner
 
     private val supportedCalculationStrategies = arrayOf(
-        CalculationStrategyHolder("Centimetro para Quilometro",CentimeterToKilometerStrategy()),
-        CalculationStrategyHolder("Quilometro para Centimetro",KilometerToCentimeterStrategy()),
-        CalculationStrategyHolder("Quilometro para Metro",KilometerToMeterStrategy()),
-        CalculationStrategyHolder("Metro para Centimetro",MeterToCentimeterStrategy()),
+        CalculationStrategyHolder("Centimetro para Quilometro", CentimeterToKilometerStrategy()),
+        CalculationStrategyHolder("Quilometro para Centimetro", KilometerToCentimeterStrategy()),
+        CalculationStrategyHolder("Quilometro para Metro", KilometerToMeterStrategy()),
+        CalculationStrategyHolder("Metro para Centimetro", MeterToCentimeterStrategy()),
         CalculationStrategyHolder("Metro para Quilometro", MeterToKilometerStrategy())
 
     )
@@ -28,23 +32,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var value  = 0.0
+        var position = 0
+
+        savedInstanceState?.let {
+            value = it.getDouble("VALUE")
+            position = it.getInt("POSITION")
+        }
+
         unitUi()
-        setUi()
+        setUi(position,value)
         setActions()
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        try {
+            outState.putDouble("VALUE", edtValue.text.toString().toDouble())
+
+        }catch (e : NumberFormatException){
+
+        }
+        outState.putInt("POSITION", spConversions.selectedItemPosition)
+    }
+
     private fun unitUi() {
         spConversions = findViewById(R.id.spConversions)
-
+        edtValue = findViewById(R.id.edtValue)
     }
 
     private fun setActions() {
-        val btnConvert : Button = findViewById(R.id.btnConvert)
-        val edtValue : EditText = findViewById(R.id.edtValue)
+        val btnConvert: Button = findViewById(R.id.btnConvert)
+        val edtValue: EditText = findViewById(R.id.edtValue)
 
         btnConvert.setOnClickListener {
 
+            //Trata o erro de tentar converter uma String vazia em Double
             try {
                 //Valor digitado no EditText
                 val value = edtValue.text.toString().toDouble()
@@ -53,32 +77,58 @@ class MainActivity : AppCompatActivity() {
                 val selectedItemPosition = spConversions.selectedItemPosition
 
                 //Pegando na mutableList criada de StrategyHolders a estrategia da posição selecionada pelo Spinner
-                val calculationStrategy = supportedCalculationStrategies[selectedItemPosition]
+                val calculationStrategy =
+                    supportedCalculationStrategies[selectedItemPosition].calculationStrategy
 
-                Calculator.setCalculationStrategy(calculationStrategy.calculationStrategy)
+                Calculator.setCalculationStrategy(calculationStrategy)
 
-                Calculator.calculate(value)
+                val result = Calculator.calculate(value)
+                val label = calculationStrategy.getResultLabel(result != 1.toDouble())
+
+                showResult(result, label)
 
 
-            }catch (e:NumberFormatException){
+            } catch (e: NumberFormatException) {
                 edtValue.error = "Valor inválido"
                 edtValue.requestFocus()
 
             }
         }
 
+        val btnClear: Button = findViewById(R.id.btnClear)
+        btnClear.setOnClickListener {
+
+            clear()
+        }
     }
 
-    private fun setUi() {
-        spConversions = findViewById(R.id.spConversions)
-        //Configurando e populando o spinner
-        val spAdapter = ArrayAdapter(this,R.layout.res_spinner_item, getSpinnerData())
-        spAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    private fun clear() {
+        edtValue.setText("")
+        edtValue.error = null
 
+        spConversions.setSelection(0)
+    }
+
+    private fun showResult(result: Double, label: String) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("RESULT", result)
+        intent.putExtra("LABEL", label)
+
+        startActivity(intent)
+    }
+
+    private fun setUi(position: Int,value :Double) {
+        spConversions = findViewById(R.id.spConversions)
+
+        //Configurando e populando o spinner
+        val spAdapter = ArrayAdapter(this, R.layout.res_spinner_item, getSpinnerData())
+        spAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spConversions.setSelection(position)
+        edtValue.setText(value.toString())
         spConversions.adapter = spAdapter
     }
 
-    private fun getSpinnerData():MutableList<String> {
+    private fun getSpinnerData(): MutableList<String> {
         val spinnerData = mutableListOf<String>()
         supportedCalculationStrategies.forEach {
             spinnerData.add(it.name)
--- app/src/main/java/com/renan/conversordemedidas/ResultActivity.kt
+++ app/src/main/java/com/renan/conversordemedidas/ResultActivity.kt
@@ -2,10 +2,25 @@ package com.renan.conversordemedidas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val result = intent.getDoubleExtra("RESULT",0.0)
+        val label = intent.getStringExtra("LABEL")
+
+        val tvResult:TextView = findViewById(R.id.tvValue)
+        tvResult.text = result.toString()
+
+        val tvLabel:TextView = findViewById(R.id.tvValueLabel)
+        tvLabel.text = label
+
+        val btnClose:Button = findViewById(R.id.btnClose)
+        btnClose.setOnClickListener { finish() }
+
     }
 }
